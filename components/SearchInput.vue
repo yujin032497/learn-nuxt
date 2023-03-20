e33f32a651bf7ca803946b9b762e46a6044055c6
YuJin,Kim <49347652+yujin032497@users.noreply.github.com>
2023-03-20 18:42:17 +0900

    Add files via upload

---
+++ components/SearchInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="input-wrapper flex">
+    <input 
+    type="text" 
+    :value="searchKeyword" 
+    class="search-input"
+    @input="$emit('input', $event.target.value)" 
+    />
+    <button class="btn" type="button" @click="$emit('search')">search</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: { // v-model은 value라고 해야한다.
+            type: String,
+            default: () => '',
+        },
+    },
+}
+</script>
+
+<style scoped>
+.input-wrapper {
+  height: 40px;
+  margin: 1rem 0;
+}
+.search-input {
+  width: 200px;
+  font-size: 1.2rem;
+  font-weight: 500;
+}
+.btn {
+  font-size: 1.2rem;
+  font-weight: 500;
+}
+</style>
