cfb420817864a4e7da76fb25c4825bce336c7d09
YuJin,Kim <49347652+yujin032497@users.noreply.github.com>
2023-03-20 18:42:46 +0900

    Add files via upload

---
+++ pages/cart.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <h1 class="list-title">카트 페이지</h1>
+    <div class="list-wrapper">
+        <ul>
+            <li 
+            v-for="cartItem in $store.state.cartItems" 
+            :key="cartItem.id"
+            class="list-item"
+            >
+                <img 
+                class="thumbnail"
+                :src="cartItem.imageUrl" 
+                :alt="cartItem.name" 
+                />
+                <div class="description">
+                    <p>{{ cartItem.name }}</p>
+                    <span>{{ cartItem.price }}</span>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="extra-panel">
+        <button>구매하기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.container {
+    margin: 2rem 10rem;
+}
+.list-title {
+    font-weight: 700;
+    font-size: 1.4rem;
+}
+.list-wrapper {
+    margin: 0.4rem 0;
+}
+.list-item {
+    display: flex;
+}
+.thumbnail {
+    width: 100px;
+    height: 100px;
+}
+.description {
+    padding: 2rem 1rem;
+}
+.extra-panel {
+    text-align: right;
+    padding: 0.2rem 0;
+}
+</style>
